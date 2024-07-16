1c65975ff945745ddd2e156271aadc3b4f0ac082
Hamza <hamza.azeem2013.ha@gmail.com>
2024-07-16 03:22:53 +0300

    adding parent-sub category relation

--- src/main/java/com/example/producttestapi/entities/Category.java
+++ src/main/java/com/example/producttestapi/entities/Category.java
@@ -6,7 +6,9 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Setter
@@ -20,10 +22,23 @@ public class Category {
     @OneToMany(mappedBy = "category")
     @JsonIgnore
     private List<Product> products;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_category_id")
+    @JsonIgnore
+    private Category parentCategory;
+    @OneToMany(mappedBy = "parentCategory", fetch = FetchType.LAZY)
+    private Set<Category> subCategories;
     public void addProduct(Product product){
         if(products == null){
             products = new ArrayList<>();
         }
         products.add(product);
+    }
+    public void addSubCategory(Category category){
+        if(subCategories == null){
+            subCategories = new HashSet<>();
+        }
+        subCategories.add(category);
+        category.setParentCategory(this);
     }
 }
